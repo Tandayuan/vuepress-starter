956497f1c2ed2ae9390d949102967a43b2312cce
谭达源 <tanleslie@foxmail.com>
2023-10-27 13:18:39 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    title: 'TDYBLog',
+    description: '简单的记录技术',
+    themeConfig: {
+        nav: [
+            { text: '我的', link: '/My/log', target: '_self', ref: '' },
+            { text: '购物车', link: '/Car/', target: '_self', ref: '' },
+        ],
+        sidebar: {
+            '/Car/': [
+                '',
+                'one',
+                'two'
+            ],
+            '/My/': [
+                'log',
+                'one',
+                'two'
+            ],
+            '/': [
+                '',
+            ]
+        } 
+    }
+}
---
+++ docs/Car/README.md
@@ -0,0 +1,5 @@
+# Car Home
+![Dio](/R-C.jpg)
+
+![JoJo](./images/README/OIP-C.jpg)
+[log link](../My/log.md)
